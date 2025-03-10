d143988cbe19f8677f8cda65999a35606db4bf9a
Keerthi Bhaskara <keerthi.bhaskara@egovernments.org>
2025-03-10 15:39:19 +0530

    TL data migration

--- exec.py
+++ exec.py
@@ -1,11 +1,11 @@
 from migrate_schema import *
 from migrate_data import *
 
-create_schema("schema/TL.json", tenantId="pg")
+#create_schema("schema/TL.json", tenantId="pg")
 # create_all_schema("schema", tenantId="as")
 # create_all_schema("schema", tenantId="as", is_portforward=False)
 
 
-# create_data("data/pqm/cityA/PQM.TestStandard.json", tenantId="pg.citya")
-# create_all_data("data/pqm", tenantId="pg.citya")
+create_data("data/TL/TradeLicense.CalculationType.json", tenantId="pg.cityc")
+#create_all_data("data/TL", tenantId="pg")
 # create_all_data("data", tenantId="pg", is_portforward=False)
