753a2f62bc25105606d1572c6ac189db053c61c9
Gurjeet-eGov <gurjeet.singh@egovernments.org>
2025-03-11 13:41:59 +0530

    fixed script

--- payload/schemaCreate.py
+++ payload/schemaCreate.py
@@ -4,8 +4,8 @@ from typing import List, Optional, Dict, Any, Union
 from payload import RequestInfo as r
 
 # Define the model for a property within the schema definition
-class SchemaProperty(BaseModel):
-    type: Union[str, List[str]]
+# class SchemaProperty(BaseModel):
+#     type: Union[str, Any, List[Any]]
 
 # Define the model for the 'definition' part of SchemaDefinition.
 # Note: We use Field with alias to handle keys that are not valid Python identifiers.
@@ -15,7 +15,7 @@ class Definition(BaseModel):
     schema_: str = Field(..., alias="$schema")
     required: List[str]
     x_unique: List[str] = Field(..., alias="x-unique")
-    properties: Dict[str, SchemaProperty]
+    properties: Dict[str, Any]
     additionalProperties: Optional[bool] = False
     x_ref_schema: List[Any] = Field(default_factory=list, alias="x-ref-schema")
 
