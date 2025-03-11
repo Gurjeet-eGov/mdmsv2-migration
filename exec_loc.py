c432dcb4b02c382a475b65ceed4e4297d6fc08c8
Gurjeet-eGov <gurjeet.singh@egovernments.org>
2025-03-12 00:06:34 +0530

    schema update

--- exec_loc.py
+++ exec_loc.py
@@ -8,10 +8,10 @@ TENANT = "pg"
 loc_obj=SchemaProcessor(path="schema/TL.json")
 loc_obj.generateLocalisations(module="rainmaker-workbench", locale="en_IN")
 
-Localisations=loc_obj.LocalisationsList
+# Localisations=loc_obj.LocalisationsList
 
 with open(FILE_PATH, 'w') as file:
-    file.write(json.dumps(Localisations))
+    file.write(json.dumps(loc_obj.LocalisationsList))
 
 migrate_localisations.upsert_localisation(FILE_PATH, TENANT)
 
