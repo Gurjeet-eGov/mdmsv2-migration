8b905e595aec7276c50f31269a6051d79f4cda8a
Gurjeet-eGov <gurjeet.singh@egovernments.org>
2025-03-17 16:56:03 +0530

    Localisation code fix

--- exec_loc.py
+++ exec_loc.py
@@ -2,16 +2,19 @@ import json
 from localisation import SchemaProcessor
 import migrate_localisations
 
-FILE_PATH = "MDMS_Localizations.json"
+OUTPUT_FILE_PATH = "MDMS_Localizations.json"
 TENANT = "pg"
+SCHEMA_PATH = "schema/pqm.json"
+MODULE_CODE = "rainmaker-workbench"
+LOCALE = "en_IN"
 
-loc_obj=SchemaProcessor(path="schema/TL.json")
-loc_obj.generateLocalisations(module="rainmaker-workbench", locale="en_IN")
+loc_obj=SchemaProcessor(path=SCHEMA_PATH)
+loc_obj.generateLocalisations(module=MODULE_CODE, locale=LOCALE)
 
 # Localisations=loc_obj.LocalisationsList
 
-with open(FILE_PATH, 'w') as file:
+with open(OUTPUT_FILE_PATH, 'w') as file:
     file.write(json.dumps(loc_obj.LocalisationsList))
 
-migrate_localisations.upsert_localisation(FILE_PATH, TENANT)
+migrate_localisations.upsert_localisation(OUTPUT_FILE_PATH, TENANT)
 
