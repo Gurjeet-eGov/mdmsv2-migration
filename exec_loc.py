23d1f8fbfb5249223acd2ec1b9cf93f919b979c9
Gurjeet-eGov <gurjeet.singh@egovernments.org>
2025-04-23 01:33:53 +0530

    added doc

--- exec_loc.py
+++ exec_loc.py
@@ -11,10 +11,12 @@ LOCALE = "en_IN"
 loc_obj=SchemaProcessor(path=SCHEMA_PATH)
 loc_obj.generateLocalisations(module=MODULE_CODE, locale=LOCALE)
 
-# Localisations=loc_obj.LocalisationsList
-
 with open(OUTPUT_FILE_PATH, 'w') as file:
     file.write(json.dumps(loc_obj.LocalisationsList))
+
+"""
+by default it takes port-forward url, ensure to port-forward localization service
+"""
 
 migrate_localisations.upsert_localisation(OUTPUT_FILE_PATH, TENANT)
 
