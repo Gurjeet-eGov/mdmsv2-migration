4e1d9b184a4ca239fac239e641ed1bd7a9043ca5
Gurjeet-eGov <gurjeet.singh@egovernments.org>
2025-04-23 01:30:54 +0530

    added docs

--- exec.py
+++ exec.py
@@ -1,6 +1,11 @@
 from migrate_schema import *
 from migrate_data import *
 
+"""
+by default the port-forward is set to true, check env_config.json
+below are exmaples for how to migrate specific schema or data file, or entire folder
+"""
+
 #create_schema("schema/TL.json", tenantId="pg")
 # create_all_schema("schema", tenantId="as")
 # create_all_schema("schema", tenantId="as", is_portforward=False)
